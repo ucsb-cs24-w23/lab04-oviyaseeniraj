00e42f2535ce1bb86a50025901aabc795a00b493
Oviya Seeniraj <oviya@engineering.ucsb.edu>
2023-02-20 00:10:18 -0800

    trying to fix remove

--- intbst.cpp
+++ intbst.cpp
@@ -397,14 +397,19 @@ bool IntBST::remove(int value)
         return false;
     } 
 
+    if (count() == 1)
+    {
+        this->root = nullptr;
+        delete n;
+        return true;
+    }
     if (!n->left && !n->right) // no children case
     {
-        if (n == this->root) // one node case
-        {
-            this->root = NULL;
-            delete n;
-        } 
-        else if (isLeftChild) 
+        // if (n == this->root) // one node case
+        // {
+        //     this->root = NULL;
+        // } 
+        if (isLeftChild) 
         {
             n->parent->left = NULL;
         } 
